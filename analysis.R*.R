e60696e5dc031295190ec30d2d5d6348c4a504d5
Scott Morrissey <scott.morrissey@my.jcu.edu.au>
2021-10-04 10:49:59 +1000

    Added more summary stats

--- analysis.R*.R
+++ analysis.R*.R
@@ -1,9 +1,5 @@
 x <- seq(1, 10, len=1)
 y <- 40*2 + rnorm(10,0,5)
 plot(x,y)
-
-#to stage file - click boxes .gitignore and analysis.R*R in git tab top left. Then click Commit and file appears under files tab.
-#opens to new window - type in commit message (always add message or hell) - click commit - gives info [master (root-commit) 4ddb67e] Initial commit -- 4dd... is a truncutated id to identify the commit. make note of this id. Is also in history tab. 
-
-#lets make a change - add summary(x) - shows that the file has been modified. - click stage - click commit - same processa as above. 
-
+summary(x)
+mean(x)
